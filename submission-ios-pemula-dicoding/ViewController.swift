add0fe62c6b4e42645ae5ab6b808ff464b3e0e0e
Jovanes Jovanotti <jovanes.jovanotti@sg-dsa.com>
2022-09-21 19:12:46 +0700

    Initial Commit

---
+++ submission-ios-pemula-dicoding/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  submission-ios-pemula-dicoding
+//
+//  Created by Jovanes Jovanotti on 21/09/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
