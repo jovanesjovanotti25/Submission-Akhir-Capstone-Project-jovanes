749b72f0aad6f7d5b79b6f002904027cf5aba41f
Jovanes Jovanotti <jovanesjovanotti@gmail.com>
2022-12-27 19:10:42 +0700

    delete space in module builder

--- submission-ios-pemula-dicoding/Core/Utils/ModuleBuilder.swift
+++ submission-ios-pemula-dicoding/Core/Utils/ModuleBuilder.swift
@@ -8,51 +8,40 @@
 import UIKit
 
 class ModuleBuilder: NSObject {
-
         class func getView(module: ModuleEnum) -> UIViewController {
             switch module {
             case .splashScreen:
                 let splashViewController = SplashViewController()
                 splashViewController.modalPresentationStyle = .fullScreen
                 return splashViewController
-
             case .main:
                 let usecase = Injection.init().provideUseCase()
                 let presenter = MainViewModel(listGamesUseCase: usecase)
-                
                 let  mainViewController = MainViewController()
                 mainViewController.presenter = presenter
                 mainViewController.modalPresentationStyle = .fullScreen
                 return mainViewController
-            
             case .profile:
                 let  profileViewController = ProfileViewController()
                 profileViewController.modalPresentationStyle = .fullScreen
                 profileViewController.title = "Profile"
                 return profileViewController
-                
             case .listFavoriteGames:
                 let usecase = Injection.init().provideUseCaseFavorite()
                 let presenter = ListFavoriteGamesViewModel(favoriteGamesUseCase: usecase)
-                
                 let  listFavoriteGamesViewController = ListFavoriteGamesViewController()
                 listFavoriteGamesViewController.presenter = presenter
                 listFavoriteGamesViewController.modalPresentationStyle = .fullScreen
                 listFavoriteGamesViewController.title = "Favorite Games"
                 return listFavoriteGamesViewController
-                
             case .detailGame:
                 let usecase = Injection.init().provideUseCaseDetail()
                 let usecasFavoritee = Injection.init().provideUseCaseFavorite()
                 let presenter = DetailGameViewModel(detailGameUseCase: usecase, favoriteGamesUseCase: usecasFavoritee)
-                
                 let  detailGameViewController = DetailGameViewController()
                 detailGameViewController.presenter = presenter
-                
                 detailGameViewController.modalPresentationStyle = .fullScreen
-                
                 return detailGameViewController
-                
             }
         }
 }
