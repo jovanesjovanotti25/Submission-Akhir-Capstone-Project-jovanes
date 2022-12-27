e8ff4bc39eaf9d93b97c1a14b8486715f5407cdd
Jovanes Jovanotti <jovanesjovanotti@gmail.com>
2022-12-27 14:02:33 +0700

    update yml

---
+++ Pods/Target Support Files/Pods-submission-ios-pemula-dicoding/Pods-submission-ios-pemula-dicoding-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_submission_ios_pemula_dicoding : NSObject
+@end
+@implementation PodsDummy_Pods_submission_ios_pemula_dicoding
+@end
---
+++ Pods/Target Support Files/Pods-submission-ios-pemula-dicodingTests/Pods-submission-ios-pemula-dicodingTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_submission_ios_pemula_dicodingTests : NSObject
+@end
+@implementation PodsDummy_Pods_submission_ios_pemula_dicodingTests
+@end
